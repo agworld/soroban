7eecb111d9225317aa7f37fcd88025892f99691b
Aaron Todd <aaron.todd@agworld.com.au>
2014-04-01 14:57:43 +0800

    Fix ISBLANK
    ISBLANK(' ') should be false, but previous implementation returned true
    
    NZONE-305 #time 5m

--- lib/soroban/functions/is_blank.rb
+++ lib/soroban/functions/is_blank.rb
@@ -1,6 +1,12 @@
 # Excel ISBLANK returns true for empty string and null
+# It doesn't return true for strings with length > 0
+#   ie ISBLANK( " " ) => false
+#      ISBLANK( "" ) => true
+#      ISBLANK( NULL ) => true
+#      ISBLANK( 0 ) => false
+#
 # It supports one cell as an input
 #
 Soroban::define :ISBLANK => lambda { |val|
-  val.to_s.blank?
+  val.to_s.length.zero?
 }
