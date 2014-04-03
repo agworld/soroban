d23bba67626aa4652d4c1ca878d6fb43fb25fb47
Aaron Todd <aaron.todd@agworld.com.au>
2014-04-03 08:31:11 +0800

    Fix up after merge

--- lib/soroban/parser.rb
+++ lib/soroban/parser.rb
@@ -10,14 +10,14 @@ module Soroban
     # A Treetop parser for Excel formulas that can generate valid Ruby expression
     # via a rewrite operation, and which can build an array of referenced labels.
     def self.instance
-    if ENV["RUBY_ENV"] == "test"
-      return @parser if @parser
-      path = Pathname.new( File.dirname( __FILE__ ) ).join( "parser/grammar" ).to_s
-      Treetop.load( path )
-      @parser ||= SorobanParser.new
-    else
-      @@parser ||= SorobanParser.new
-    end
+      if ENV["RUBY_ENV"] == "test"
+        return @parser if @parser
+        path = Pathname.new( File.dirname( __FILE__ ) ).join( "parser/grammar" ).to_s
+        Treetop.load( path )
+        @parser ||= SorobanParser.new
+      else
+        @@parser ||= SorobanParser.new
+      end
     end
   end
 
